7261cd0b99b19c035049f7ee1c470c809b311b7f
furkanozmen <ozmenf97@gmail.com>
2022-07-17 13:55:35 +0300

    fixed return types

--- src/main/java/com/example/cqrs/CqrsApplication.java
+++ src/main/java/com/example/cqrs/CqrsApplication.java
@@ -4,14 +4,21 @@ import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Optional;
+import java.util.concurrent.ScheduledExecutorService;
 
 @SpringBootApplication
 public class CqrsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CqrsApplication.class, args);
+    }
+
+    @Bean(name = "taskExecutor")
+    public ScheduledExecutorService scheduledExecutorService() {
+        return java.util.concurrent.Executors.newScheduledThreadPool(1);
     }
 
 }
--- src/main/java/com/example/cqrs/elastic/ElasticListener.java
+++ src/main/java/com/example/cqrs/elastic/ElasticListener.java
@@ -3,12 +3,15 @@ package com.example.cqrs.elastic;
 import com.example.cqrs.model.Publication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ScheduledExecutorService;
+
 @Service
 public record ElasticListener(ElasticSearchService elasticsearchService,
-                              ObjectMapper objectMapper) {
+                              ObjectMapper objectMapper, @Qualifier("taskExecutor") ScheduledExecutorService exetutorService) {
 
     private static byte RETRY_COUNT = 0;
 
@@ -21,11 +24,12 @@ public record ElasticListener(ElasticSearchService elasticsearchService,
             elasticsearchService.createIndex("publications", publication.getId(), publication);
             RETRY_COUNT++;
         } catch (Exception e) {
-           if (RETRY_COUNT < 3) {
-               listen(event);
+           if (RETRY_COUNT <= 3) {
+               exetutorService.scheduleAtFixedRate(() -> listen(event), RETRY_COUNT, RETRY_COUNT, java.util.concurrent.TimeUnit.SECONDS);
            } else {
                LOGGER.info("Failed to save publication");
                e.printStackTrace();
+               exetutorService.shutdown();
            }
         }
     }
